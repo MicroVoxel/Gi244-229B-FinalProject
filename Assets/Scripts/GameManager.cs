f4035104dcc6557d80ff8dbf17ced91c43cf7d24
MicroVoxel <machenga.koko@gmail.com>
2025-05-12 01:23:51 +0700

    UI
    make gamePause menu and distance score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,12 +7,17 @@ public class GameManager : MonoBehaviour
     public GameObject gameStart;
     public GameObject gameEnd;
     public GameObject gamePlay;
+    public GameObject gamePause;
+
+    public int distance = 0;
+    public TextMeshProUGUI disText;
 
     private void Awake()
     {
         gameStart.SetActive(true);
         gameEnd.SetActive(false);
         gamePlay.SetActive(false);
+        gamePause.SetActive(false);
         Time.timeScale = 0;
     }
 
@@ -24,12 +30,29 @@ public class GameManager : MonoBehaviour
             gamePlay.SetActive(false);
             StopAllCoroutines();
         }
+        else
+        {
+            distance += 1;
+            disText.text = distance.ToString();
+        }
     }
 
     public void StartGame()
     {
         gameStart.SetActive(false);
         gamePlay.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void PauseGame()
+    {
+        gamePause.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        gamePause.SetActive(false);
         Time.timeScale = 1;
     }
 
